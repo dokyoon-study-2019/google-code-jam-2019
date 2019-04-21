a90dc8062fc0164bb5993991853fc687d93d85f3
DokyoonKim <uhug@naver.com>
2019-04-21 23:27:00 +0900

    clear q3

---
+++ Qr1.java
@@ -0,0 +1,13 @@
+
+public class Qr1 {
+
+    public static void main(String args[]){
+
+        System.out.println("asdf");
+
+
+
+
+
+    }
+}
--- qr-1.swift
+++ qr-1.swift
@@ -1,5 +1,58 @@
 
 
+let max : Int64 = 1000000000000000
+
+var tmp : Bool = true
+
+
+// print(2);
+// for i in 12163...max {
+
+//     for j in 2...(i-1) {
+        
+//         if i%j == 0 {
+//             tmp = false
+//             continue
+//         }
+
+//     }
+//     if tmp && i > 10000 {
+//         print(i);
+//     } else {
+//         tmp = true
+//     }
+// }
+
+var test: Int = 105
+
+var a : Int = test
+var b: Int = 2
+
+if a%b == 0{
+    a = a/b
+} else {
+    b = b + 1
+
+    for _ in b...a {
+        if(a%b == 0){
+            a = a/b
+            break
+        }
+        b = b + 2
+    }
+
+}
+
+print("a: \(a), b: \(b)")
+
+
+
+
+
+
+
+
+
 
 var optT: Int? = Int(readLine()!)
 // Stringì Intíìì¼ë¡ ë°ê¿ ë,
@@ -7,6 +60,11 @@ var optT: Int? = Int(readLine()!)
 
 var T: Int = optT!
 var N: [Int] = []
+
+
+
+
+
 
 for _ in 1...T {
     
